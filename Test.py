38d564c35998342439152002f35bf9f6ed29ef5c
Avantha De Silva <avantha.desilva@gmail.com>
2015-10-19 21:29:40 +0800

    Training file calculation complete for n number of inputs The iterations are calculated based on the target error

--- Test.py
+++ Test.py
@@ -1,12 +1,41 @@
 __author__ = 'Avantha'
 
-tst = open('data_train/train_inputs.csv','r')
-text = tst.read()
-tst.close()
-lst = text.split('\n')
-lst= [i for i in lst if i != '']
-lst = list(map(float, lst))
-print(lst)
+ads_neurons_n = 3
 
+file_inputs = open('data_train/train_inputs.csv', 'r')
+text_t_in = file_inputs.read()
+file_inputs.close()
+file_outputs = open('data_train/train_outputs.csv', 'r')
+text_t_out = file_outputs.read()
+file_outputs.close()
 
+t_in_lst = text_t_in.split('\n')
+t_in_lst = [i for i in t_in_lst if i != '']
 
+t_out_lst = text_t_out.split('\n')
+t_out_lst = [i for i in t_out_lst if i != '']
+
+# define the number if input and output weights
+in_n = t_in_lst[0].split(',')
+ot_n = t_out_lst[0].split(',')
+
+in_lyr_nw = ads_neurons_n * len(in_n)
+out_lyr_nw = ads_neurons_n * len(ot_n)
+print(in_lyr_nw)
+print(out_lyr_nw)
+
+if len(t_in_lst) != len(t_out_lst):
+    print('Data mismatch, number of inputs does not match number of outputs')
+else:
+    for n in range(0, len(t_in_lst)):
+        list1 = t_in_lst[n].split(',')
+        list2 = t_out_lst[n].split(',')
+        list1 = [i for i in list1 if i != '']  # remove null values
+        list2 = [i for i in list2 if i != '']
+        list1 = list(map(float, list1))  # convert string to float so that calculations can take place
+        list2 = list(map(float, list2))
+        if len(list1) != len(in_n) or len(list2) != len(ot_n):
+            print('Data mismatch, one or many inputs/out puts has missing data')
+        else:
+            print(list1[0])
+            print(list2[0])
--- TrainNet.py
+++ TrainNet.py
@@ -5,85 +5,111 @@ from CommonClassfn import DynamicNeuralVal
 from CommonClassfn import NewWCalc
 from CommonClassfn import HidnErroCalc
 
-# user inputs !!! Makesure the same function is not called twice this will create more lists !!!!
-in_out_file = open('train_in_outs.csv', 'r')
-in_out_text = in_out_file.read()  # read the input file and assign to text
-
-f_list = in_out_text.split('\n')  # break the two line to input and output
-in_out_file.close()
-list1 = f_list[0].split(',')  # split the values using the ','
-list2 = f_list[1].split(',')
-list1 = [i for i in list1 if i != '']  # remove null values
-list2 = [i for i in list2 if i != '']
-list1 = list(map(float, list1))  # convert string to float so that calculations can take place
-list2 = list(map(float, list2))
-ads_input_vals = list1
-ads_target_vals = list2
-
-
 ads_neurons_n = 3  # number of Neurons required - Fix
 ads_lrn_rate = 1  # enter learning rate - Fix
-ads_itn_n = 1000  # number of iterations required
-ads_itn_cntr = 1
-ads_tgt_error = 0.0001
+ads_itn_n = 4  # number of iterations required
+ads_itn_cntr = 1  # do not change manually
+ads_tgt_error = 0.01
 ads_in_ws = []
 ads_out_ws = []
 ads_abs_error = []
+ads_numberof_itn = 1
 
-# Creating the initial Ws
+# user inputs !!! Makesure the same function is not called twice this will create more lists !!!!
+file_inputs = open('data_train/train_inputs.csv', 'r')
+text_t_in = file_inputs.read()
+file_inputs.close()
+file_outputs = open('data_train/train_outputs.csv', 'r')
+text_t_out = file_outputs.read()
+file_outputs.close()
+
+t_in_lst = text_t_in.split('\n')
+t_in_lst = [i for i in t_in_lst if i != '']
+
+t_out_lst = text_t_out.split('\n')
+t_out_lst = [i for i in t_out_lst if i != '']
+
+# ----------define the number if input and output weights--------
+in_n = t_in_lst[0].split(',')
+ot_n = t_out_lst[0].split(',')
+
+in_lyr_nw = ads_neurons_n * len(in_n)
+out_lyr_nw = ads_neurons_n * len(ot_n)
+
 i = 1
 j = 1
-
-while i <= len(ads_input_vals) * ads_neurons_n:
+while i <= in_lyr_nw:
     ads_in_ws.append(round(random.random(), 3))
     i += 1
-
-while j <= len(ads_target_vals) * ads_neurons_n:
+while j <= out_lyr_nw:
     ads_out_ws.append(round(random.random(), 3))
     j += 1
-
-while ads_itn_cntr <= ads_itn_n:
-    # forward pass 1st iteration C1 - <class><number>
-    ads_C1 = DynamicNeuralVal(ads_input_vals, ads_neurons_n, ads_target_vals, ads_in_ws,
-                              ads_out_ws)  # pass the inputs to the neuron calc class
-    ads_nvals = ads_C1.dum_n_cal()
-    ads_outvals = ads_C1.dum_out_cal()
-    ads_out_errs = ads_C1.dum_o_error()
+# ---------------------------------------------------
 
 
-    # back propagation output layer weights correction
-    ads_output_ws = ads_C1.ouw
-    ads_C2 = NewWCalc(ads_output_ws, ads_out_errs, ads_nvals, ads_lrn_rate)
-    ads_new_ow = ads_C2.dum_w_new()
+if len(t_in_lst) != len(t_out_lst):
+    print('Data mismatch, number of inputs does not match number of outputs')
+else:
+    while ads_itn_cntr >= ads_tgt_error:
+        ads_temp_err2 = []  # temp error to store the absolute data list error to write to the file
+        for n in range(0, len(t_in_lst)):
+            list1 = t_in_lst[n].split(',')
+            list2 = t_out_lst[n].split(',')
+            list1 = [i for i in list1 if i != '']  # remove null values
+            list2 = [i for i in list2 if i != '']
+            list1 = list(map(float, list1))  # convert string to float so that calculations can take place
+            list2 = list(map(float, list2))
+            if len(list1) != len(in_n) or len(list2) != len(ot_n):
+                print('Data mismatch, one or many inputs/out puts has missing data')
+            else:
+                ads_input_vals = list1
+                ads_target_vals = list2
 
 
-    # Back propagation hidden layer error calculation
-    ads_new_w = ads_C2.new_w
-    ads_C3 = HidnErroCalc(ads_nvals, ads_new_w, ads_out_errs)
-    ads_hdnl_errs = ads_C3.out_errors()
+                # forward pass 1st iteration C1 - <class><number>
+                # pass the inputs to the neuron calc class
+                ads_C1 = DynamicNeuralVal(ads_input_vals, ads_neurons_n, ads_target_vals, ads_in_ws, ads_out_ws)
+                ads_nvals = ads_C1.dum_n_cal()
+                ads_outvals = ads_C1.dum_out_cal()
+                ads_out_errs = ads_C1.dum_o_error()
 
 
-    # back propagation input layer weights correction
-    ads_input_ws = ads_C1.inw
-    ads_C4 = NewWCalc(ads_input_ws, ads_hdnl_errs, ads_input_vals, ads_lrn_rate)
-    ads_new_inw = ads_C4.dum_w_new()
+                # back propagation output layer weights correction
+                ads_output_ws = ads_C1.ouw
+                ads_C2 = NewWCalc(ads_output_ws, ads_out_errs, ads_nvals, ads_lrn_rate)
+                ads_new_ow = ads_C2.dum_w_new()
 
-    ads_tmp_err = []
-    for x in ads_out_errs:
-        ads_tmp_err.append(abs(x))
-    ads_abs_error.append(sum(ads_tmp_err))
-    del ads_tmp_err[:]
 
-    ads_in_ws = ads_new_inw
-    ads_out_ws = ads_new_ow
+                # Back propagation hidden layer error calculation
+                ads_C3 = HidnErroCalc(ads_nvals, ads_new_ow, ads_out_errs)
+                ads_hdnl_errs = ads_C3.out_errors()
 
-    ads_itn_cntr += 1
+                # back propagation input layer weights correction
+                ads_input_ws = ads_C1.inw
+                ads_C4 = NewWCalc(ads_input_ws, ads_hdnl_errs, ads_input_vals, ads_lrn_rate)
+                ads_new_inw = ads_C4.dum_w_new()
+
+                ads_in_ws = ads_new_inw
+                ads_out_ws = ads_new_ow
+
+                ads_temp_err1 = []
+                for i in ads_out_errs:
+                    ads_temp_err1.append(abs(i))
+                ads_temp_err2.append(sum(ads_temp_err1))
+                del ads_temp_err1[:]
+
+        ads_abs_error.append(sum(ads_temp_err2) / len(ads_temp_err2))
+        del ads_temp_err2[:]
+
+        ads_numberof_itn += 1
+        ads_itn_cntr = ads_abs_error[-1]
 
 
 print('calculation complete')
+print('Number of iterations performed:', ads_numberof_itn)
 # -----------------------------------
 # Creating the weights file
-ads_w_file = 'ads_trained weights.txt'
+ads_w_file = 'data_cnfg/ads_trained weights.txt'
 w_file = open(ads_w_file, 'w')
 w_file.write(str(ads_in_ws) + '\n')
 w_file.write(str(ads_out_ws) + '\n')
@@ -95,7 +121,7 @@ start_err = ads_abs_error[0]
 end_err = ads_abs_error[-1]
 print(start_err)
 print(end_err)
-ads_file = 'Ads_Err_Reduction.csv'
+ads_file = 'analysis/Ads_Err_Reduction.csv'
 fx = open(ads_file, 'w')
 for x in ads_abs_error:
     fx.write(str(x) + '\n')
@@ -103,10 +129,10 @@ fx.close()
 print('Abs Error file write complete')
 
 # Creating setup information
-ads_config_file = 'ads_train_config_data.txt'
+ads_config_file = 'data_cnfg/ads_train_config_data.txt'
 w_config = open(ads_config_file, 'w')
-w_config.write(str(len(ads_input_vals)) + '\n')
-w_config.write(str(len(ads_target_vals)) + '\n')
+w_config.write(str(len(in_n)) + '\n')
+w_config.write(str(len(ot_n)) + '\n')
 w_config.write(str(ads_neurons_n) + '\n')
 w_config.write(str(len(ads_in_ws)) + '\n')
 w_config.write(str(len(ads_out_ws)) + '\n')
